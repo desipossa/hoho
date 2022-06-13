184a2ac8bfd20777fdaf557921c8a3b23b487f60
desipossa <100889937+desipossa@users.noreply.github.com>
2022-06-13 21:52:03 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,12 +8,32 @@ $(function () {
         $('.top_banner').slideUp();
     });
 
+    $('.main_slider').on('init afterChange', function(e,s,c){
+        //console.log(e,s,c);//0,1,2
+        //$('.main_slider figure').eq(c+1).addClass('on').siblings().removeClass('on');
+        let current = $('.slick-current');
+        current.addClass('on').siblings().removeClass('on');
+        $('.slideNum').text((c ? c+1 : 1) + "/" + s.slideCount)
+    });
+
     $('.main_slider').slick({
+        arrows: false,
         autoplay: true,
         dots: true,
         pauseOnHover: false,
         pauseOnFocus: false,
     });
+
+    $('.slideArrows i:nth-child(1)').on('click', function(){
+        $('.main_slider').slick('slickPrev')
+    });
+
+    $('.slideArrows i:nth-child(2)').on('click', function(){
+        $('.main_slider').slick('slickNext')
+    })
+
+
+
 
 })
 
